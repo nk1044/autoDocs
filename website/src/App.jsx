e75b246f830027d3095134386ffea0855363f825
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-04-04 23:56:44 +0530

    remove App.css and implement routing with Home and Layout components

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './Pages/Home';
+import Layout from './Pages/Layout';
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Layout />,
+  errorElement: <h1>404</h1>,
+  children: [
+    {path: '', element: <Home />},
+  ]
+}]);
 
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
