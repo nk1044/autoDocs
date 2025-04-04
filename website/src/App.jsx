5f5dc06fbe357cfc87f6511b9be203caf4fdfb92
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-04-05 00:02:30 +0530

    add 404 page component and update routing to handle non-existent paths

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from 'react';
 import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import Home from './Pages/Home';
 import Layout from './Pages/Layout';
+import Page404 from './Pages/Page404';
 
 const router = createBrowserRouter([{
   path: '/',
   element: <Layout />,
-  errorElement: <h1>404</h1>,
   children: [
     {path: '', element: <Home />},
   ]
-}]);
+},
+{path: '*', element: <Page404 />},
+]);
 
 
 function App() {
