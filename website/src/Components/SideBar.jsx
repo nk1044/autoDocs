ed6dc39fa3ec53ba1ef49e6bb05169ec41139851
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-04-05 10:32:23 +0530

    refactor Header and SideBar components for improved layout and styling

--- website/src/Components/SideBar.jsx
+++ website/src/Components/SideBar.jsx
@@ -9,12 +9,13 @@ function SideBar() {
   const [active, setActive] = useState('')
 
   return (
-      <div className='flex flex-col h-full bg-neutral-800 border rounded-r-2xl border-neutral-700'>
-        <div className='p-4 text-2xl font-bold'>Docs</div>
+      <div className='flex flex-col h-full bg-neutral-900 border rounded-r-2xl border-neutral-700'>
         <div className='flex flex-col'>
           {docs.map((doc, index) => (
-            <Link to={`/docs/${doc}`} key={index} className={`p-4 hover:bg-neutral-700 ${active === doc ? 'bg-neutral-700' : ''}`} onClick={() => setActive(doc)}>
-              {doc}
+            <Link to={`/docs/${doc}`} key={index} className={`px-4 py-1 text-lg  cursor-pointer transform hover:scale-105 transition-transform duration-200 ${active === doc ? 'bg-neutral-700' : ''}`} onClick={() => setActive(doc)}>
+              <div className='border-b border-neutral-700'>
+                {doc}
+              </div>
             </Link>
           ))}
         </div>
--- website/src/Pages/Layout.jsx
+++ website/src/Pages/Layout.jsx
@@ -5,7 +5,7 @@ import Header from '../Components/Header'
 
 function Layout() {
   return (
-    <div className='flex flex-col gap-1 h-screen bg-neutral-900'>
+    <div className='flex flex-col gap-2 h-screen bg-neutral-900'>
       <Header />
       <div className='flex flex-row flex-grow h-full'>
       <div className='w-1/4 '>
