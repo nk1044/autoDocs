a447cf5aa55d1cc3f9832bc9c30a47050e3d2f23
NeerajKumar-1044 <neerajkumar.cs1044@gmail.com>
2025-04-05 10:37:24 +0530

    refactor SideBar component to use NavLink for improved navigation and styling

--- website/src/Components/SideBar.jsx
+++ website/src/Components/SideBar.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import {data} from '../../docsData.js';
+import { NavLink } from 'react-router-dom';
+import { data } from '../../docsData.js';
 
 function SideBar() {
-
-  const [docs, setDocs] = useState(data.SideBar || []);
-  const [active, setActive] = useState('')
+  const docs = data.SideBar || [];
 
   return (
-      <div className='flex flex-col h-full bg-neutral-900 border rounded-r-2xl border-neutral-700'>
-        <div className='flex flex-col'>
-          {docs.map((doc, index) => (
-            <Link to={`/docs/${doc}`} key={index} className={`px-4 py-1 text-lg  cursor-pointer transform hover:scale-105 transition-transform duration-200 ${active === doc ? 'bg-neutral-700' : ''}`} onClick={() => setActive(doc)}>
-              <div className='border-b border-neutral-700'>
+    <div className='flex flex-col h-full bg-neutral-900 border rounded-r-2xl border-neutral-700'>
+      <div className='flex flex-col'>
+        {docs.map((doc, index) => (
+          <NavLink
+            to={`/docs/${doc}`}
+            key={index}
+            className={({ isActive }) =>
+              `px-4 py-1 text-xl cursor-pointer transform hover:scale-105 transition-transform duration-200  ${
+                isActive ? 'text-orange-600' : ''
+              }`
+            }
+          >
+            <div className='border-b px-1 border-neutral-700'>
                 {doc}
               </div>
-            </Link>
-          ))}
-        </div>
+          </NavLink>
+        ))}
       </div>
-  )
+    </div>
+  );
 }
 
-export default SideBar
+export default SideBar;
